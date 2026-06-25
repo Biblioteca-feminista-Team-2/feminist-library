e7754cf299b19caee31f44c9a2a15e8bc2bc01b0
Andrea Tapia <a.tapia.mallea@gmail.com>
2026-06-25 12:46:25 +0200

    add mocking data-bookcontroller (#8)

--- src/main/java/com/library/controller/BookController.java
+++ src/main/java/com/library/controller/BookController.java
@@ -1,17 +1,46 @@
 package com.library.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import com.library.model.Book;
 import com.library.repository.BookRepository;
+
 
 public class BookController {
 
     private BookRepository bookRepository;
 
-    public BookController(BookRepository bookRepository){
+    public BookController(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
 
-    public void createBook(Book book){
+    public void createBook(Book book) {
         bookRepository.createBook(book);
+    }
+
+    public List<Book> getAllBooks() {
+        List<Book> mockList = new ArrayList<>();
+
+    }
+
+    public void updateBook(Book book) {
+
+    }
+    public void deleteBook(int id) {
+    }
+
+    public List<Book> searchByTitle(String title) {
+        return new ArrayList<>();
+    }
+
+    public List<Book> searchByAuthor(String authorName) {
+        return new ArrayList<>();
+    }
+
+    public List<Book> searchByGenre(String genreName) {
+        return new ArrayList<>();
+
     }
 
 }
