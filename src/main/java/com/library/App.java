51f7227af81f73858c6db2744a727fae9aa0c939
atapiamallea <a.tapia.mallea@gmail.com>
2026-06-26 10:31:27 +0200

    fix: merge conflict error

--- src/main/java/com/library/App.java
+++ src/main/java/com/library/App.java
@@ -1,19 +1,17 @@
 package com.library;
-import java.util.Scanner;
 
 import com.library.repository.BookRepositoryImpl;
 import com.library.controller.BookController;
 import com.library.repository.BookRepository;
-import com.view.ConsoleView;
+import com.view.MainMenuView;
 public class App {
     public static void main(String[] args) {
 
         BookRepository bookRepository = new BookRepositoryImpl();
         BookController bookController = new BookController(bookRepository);
-        ConsoleView bookView = new ConsoleView(bookController);
-        Scanner scanner = new Scanner(System.in);
+        MainMenuView bookView = new MainMenuView(bookController);
 
-        bookView.createBook(scanner);
+        bookView.start();
 
     }
 
